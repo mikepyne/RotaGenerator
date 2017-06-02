bb236b9ba4d3ac2cabda49566c685e8156cd5210
Mike Pyne <mike@pyne.me.uk>
2017-06-02 15:15:58 +0100

    Added additional command line parameters

--- rota/main.py
+++ rota/main.py
@@ -7,7 +7,11 @@ from .config import Config, ConfigException
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Do some rota magic')
     parser.add_argument('start', type=int, help="Starting month")
+    parser.add_argument('start_year', type=int, help="Starting year")
     parser.add_argument('end', type=int, help="Ending month")
+    parser.add_argument('-e', '--end_year', type=int, help="Ending month, if different",
+                        nargs='?', default=None)
+
 
     args = parser.parse_args()
     r = RotaDays()
@@ -22,4 +26,5 @@ if __name__ == '__main__':
     except ConfigException as e:
         print ('Error: {0}'.format(e.msg))
 
-    print('\n'.join(r.find_weekends(2017, args.start, args.end)))
+    print('\n'.join(r.find_weekends(args.start_year, args.start, args.end,
+                                    args.end_year)))
