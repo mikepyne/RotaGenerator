dc4ee02498083f33ff4d919b6a9733e6947c301b
Mike Pyne <mike@pyne.me.uk>
2017-06-05 12:28:57 +0100

    Use pytest instead of unittest

--- tests/test_rota_days.py
+++ tests/test_rota_days.py
@@ -1,28 +1,30 @@
-import unittest
+import pytest
 from datetime import date
 from rota.rota_days import RotaDays
 
-class TestRotaDays(unittest.TestCase):
-    def setUp(self):
-        self.rd = RotaDays()
 
-    def test_format_weekend(self):
+@pytest.fixture(scope="module")
+def rd():
+    return RotaDays()
+
+
+class TestRotaDays(object):
+    def test_format_weekend(self, rd):
         Saturday = date(2016, 12, 31)
         Sunday = date(2017, 1, 1)
         expected = 'Saturday 31st December/Sunday 1st January'
+        assert rd.format_weekend(Saturday, Sunday) == expected
 
-        self.assertEqual(self.rd.format_weekend(Saturday, Sunday), expected)
-
-    def test_find_weekends_1(self):
+    def test_find_weekends_1(self, rd):
         expected = ['Saturday 31st December/Sunday 1st January',
                     'Saturday 7th/Sunday 8th January',
                     'Saturday 14th/Sunday 15th January',
                     'Saturday 21st/Sunday 22nd January',
                     'Saturday 28th/Sunday 29th January']
 
-        self.assertEqual(self.rd.find_weekends(2017, 1, 1), expected)
+        assert rd.find_weekends(2017, 1, 1) == expected
 
-    def test_find_weekends_2(self):
+    def test_find_weekends_2(self, rd):
         expected = ['Saturday 28th February/Sunday 1st March',
                     'Saturday 7th/Sunday 8th March',
                     'Saturday 14th/Sunday 15th March',
@@ -38,9 +40,9 @@ class TestRotaDays(unittest.TestCase):
                     'Saturday 23rd/Sunday 24th May',
                     'Saturday 30th/Sunday 31st May']
 
-        self.assertEqual(self.rd.find_weekends(2015, 3, 5), expected)
+        assert rd.find_weekends(2015, 3, 5) == expected
 
-    def test_find_weekends_dec_feb(self):
+    def test_find_weekends_dec_feb(self, rd):
         # Liturgical year starts in December, so first rota period is Dec - Feb
         expected = ['Saturday 5th/Sunday 6th December',
                     'Saturday 12th/Sunday 13th December',
@@ -56,12 +58,12 @@ class TestRotaDays(unittest.TestCase):
                     'Saturday 20th/Sunday 21st February',
                     'Saturday 27th/Sunday 28th February']
 
-        self.assertEqual(self.rd.find_weekends(2015, 12, 2, 2016), expected)
+        assert rd.find_weekends(2015, 12, 2, 2016) == expected
 
-    def test_find_weekends_bad_params(self):
+    def test_find_weekends_bad_params(self, rd):
         expected = []
 
-        self.assertEqual(self.rd.find_weekends(2015, 5, 3), expected)
+        assert rd.find_weekends(2015, 5, 3) == expected
 
 
 if __name__ == '__main__':
