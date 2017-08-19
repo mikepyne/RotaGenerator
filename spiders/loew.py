3824c3c49a03746e644555605fecd997c0e59a0c
Mike Pyne <mike@pyne.me.uk>
2017-08-19 23:52:03 +0100

    xpath targets only Sunday rows
    Note that Sunday rows might include days that aren't Sundays
    (e.g. Tuesday 15th August 2017 - Assumption; classed as Sunday)

--- spiders/loew.py
+++ spiders/loew.py
@@ -1,10 +1,17 @@
+import logging
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
+from scrapy.utils.log import configure_logging
 
 
 class LoewSpider(scrapy.Spider):
+    """Spider for extracting data from the Liturgy Office calendar
+
+    I've noticed that the calendar uses the 'Sunday' class for days other than
+    sundays. For example, Tuesday 15th August 2017 has the class 'Sunday'
+    """
     name = "liturgyoffice"
 
     def start_requests(self):
@@ -15,15 +22,21 @@ class LoewSpider(scrapy.Spider):
 
     def parse(self, response):
         html = HtmlResponse(
-            url='http://www.liturgyoffice.org.uk/Calendar/2017/Aug.shtml',
+            url=response.url,
             body=response.body)
 
-        print('Selector: {0}'.format(Selector(response=html).xpath(
+        rows = Selector(response=html).xpath(
             '//html/body/div[@id="wrapper"]/div[@id="header"]/div[@id="main"]'
             '/div[@id="content"]/div[@id="Calendar"]'
-        ).extract()))
+            '/table[@summary="Liturgical Calendar for August 2017"]/tbody/tr'
+            '[td[@class="Sunday"]]'
+        )
+
+        for row in rows:
+            print(row.extract())
 
 
+configure_logging({'LOG_LEVEL': logging.ERROR})
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
 })
